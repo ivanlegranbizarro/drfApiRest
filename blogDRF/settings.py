5507daa6d27779a0b816c0c30653ed21d0793811
Iván Legrán <ivanlegranbizarro@gmail.com>
2023-01-16 11:33:21 +0100

    fixing typos

--- blogDRF/settings.py
+++ blogDRF/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'blogDRF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': config['DB_ENGINE'],
         'NAME': config['DB_NAME'],
         'USER': config['DB_USER'],
         'PASSWORD': config['DB_PASSWORD'],
