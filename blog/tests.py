d263664afac8fef16bf7ba6899fd12191c7aa866
Iván Legrán <ivanlegranbizarro@gmail.com>
2023-01-19 09:49:18 +0100

    setup testing

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
+from .models import Articulo
 
 # Create your tests here.
 
-# TODO: Instalar Pytest
+class BlogTests(TestCase):
+  def test_post_str(self):
+    articulo = Articulo(nombre='A sample title')
+    self.assertEqual(str(articulo), articulo.nombre)
+
+
