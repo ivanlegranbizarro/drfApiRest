0ceb76730a11683718194f9741c447490861dcce
Iván Legrán <ivanlegranbizarro@gmail.com>
2023-01-23 09:13:01 +0100

    herencia de plantillas

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('categorias/<int:pk>/', views.CategoriaDetailAndCreateView.as_view()),
     path('articulos/', views.ArticuloView.as_view()),
     path('articulos/<int:pk>/', views.ArticuloDetailAndCreateView.as_view()),
-    path('articulos/list/', views.ArticuloListView.as_view(), name='articulo_list'),
+    path('articulos-list', views.ArticuloListView.as_view(), name='articulo_list'),
 ]
