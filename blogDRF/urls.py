b6f1bdf8c9ca0045e8df5286573ce844775001f7
Iván Legrán <ivanlegranbizarro@gmail.com>
2023-01-23 10:16:04 +0100

    fixing urls

--- blogDRF/urls.py
+++ blogDRF/urls.py
@@ -21,10 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/',
-         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/',
-         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('', SpectacularSwaggerView.as_view(
         url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(
+        url_name='schema'), name='redoc'),
 ]
