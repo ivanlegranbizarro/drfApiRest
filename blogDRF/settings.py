07e2760d1ecaf347fdbc25ee559976616e9c0ad6
Iván Legrán <ivanlegranbizarro@gmail.com>
2023-01-23 10:09:07 +0100

    documentant api

--- blogDRF/settings.py
+++ blogDRF/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'rest_framework',
+    'drf_spectacular',
 
 ]
 
@@ -133,3 +134,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Django Masterclass IT-Academy',
+    'DESCRIPTION': 'Una clase para atarlos a todos y reunirlos en las tinieblas',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False
+}
